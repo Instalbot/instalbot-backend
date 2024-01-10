64ab4634dfd7a24a85d794a8c9e7f2316b4600ad
Nimplex <39964594+Nimplex@users.noreply.github.com>
2024-01-10 22:34:30 +0100

    use one handler for route

--- backend/api/settings/urls.py
+++ backend/api/settings/urls.py
@@ -1,17 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_jwt_extended import jwt_required
 from .controllers import get_settings, update_settings
 
 settingsBlueprint = Blueprint('settings', __name__)
 
 
-@settingsBlueprint.get('/')
+@settingsBlueprint.route('/', methods=['GET', 'PUT'])
 @jwt_required()
-def index():
-    return get_settings()
-
-
-@settingsBlueprint.put('/')
-@jwt_required()
-def update():
-    return update_settings()
+def handle_settings():
+    if request.method == 'GET':
+        return get_settings()
+    elif request.method == 'PUT':
+        return update_settings()
