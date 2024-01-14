fd034bbde79089b398a27d2a07f6423ea57a22b4
Nimplex <39964594+Nimplex@users.noreply.github.com>
2024-01-15 00:12:40 +0100

    remove everything (again)

--- frontend/src/app/navbar/navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-    return (
-        <div id="navbar" className="bg-neutral-900 p-1 min-h-9">
-            <h1 className="font-semibold text-sm">InstalBot</h1>
-        </div>
-    )
-}
